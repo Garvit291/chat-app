a3f3f4cde5d3f5502ba46b3d179caeb64789c63e
Garvit291 <chhabragarvit291@outlook.com>
2020-06-04 16:31:31 +0530

    created server for sending admin message and other functions for user and on front end infobar is designed other component left

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React ,{ useState , useEffect } from "react";
+import queryString from  'query-string';
+import io from 'socket.io-client';
+import './Chat.css';
+import Infobar from './../Infobar/Infobar.js';
+let socket;
+
+
 
 const Chat = () =>{
+
+    const ENDPOINT='localhost:5000';
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message,setMessage]=useState('');
+    const [messages,setMessages]=useState([]);
+
+    useEffect(()=>{
+        const {name,room}= queryString.parse(window.location.search);
+        setName(name)
+        setRoom(room)
+
+        socket=io(ENDPOINT);
+        socket.emit('join',{name,room},(error)=>{
+            if(error) {
+                alert(error);
+              }
+        })
+
+        return () =>{
+            socket.emit('disconnect');
+            socket.off();
+        }
+    },[ENDPOINT,window.location.search]);
+
+    useEffect(()=>{
+        socket.on('message',(message)=>{
+            setMessages([...messages,message]);
+
+        })
+    },[messages]);
+
+    const sendMessage=(event) =>{
+        event.preventDefault();
+    
+        if(message){
+            socket.emit('sendMessage',message,()=>setMessage(''));
+        }
+    }
+    console.log(message,messages);
+
+
     return(
-        <h1> Chat </h1>
+        <div className='outerContainer'>
+            <div className='container'>
+            <Infobar room={room}/>
+            </div>
+        </div>
+        
     );
 }
 
---
+++ frontend/src/components/Infobar/Infobar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Infobar.css';
+const closeIcon='https://raw.githubusercontent.com/adrianhajdin/project_chat_application/master/client/src/icons/closeIcon.png';
+const onLIneIcon='https://raw.githubusercontent.com/adrianhajdin/project_chat_application/master/client/src/icons/onlineIcon.png'
+
+
+const Infobar = ({room}) =>{
+    return(
+    <div className='infoBar'>
+        <div className='leftInnerContainer'>
+            <img className='onlineIcon' src={onLIneIcon} alt="online icon"/>
+            <h3> {room}  </h3>
+        </div>
+        <div className='rightInnerContainer'>
+        <a href='/'><img src={closeIcon} alt="close "/></a>
+        </div>
+    </div>
+    );
+
+}
+
+export default Infobar;
