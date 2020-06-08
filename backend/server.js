b70dd456d1b43493c42f2c26ff3958ea460a3dd9
Garvit291 <chhabragarvit291@outlook.com>
2020-06-08 16:06:48 +0530

    designed footer some links left

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http= require('http');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000 ;
 const app = express();
+app.use(cors());
 app.use(express.json())
 const {addUser,removeUser,getUser,getUsersInRoom} =require('./Users')
 
@@ -19,14 +21,12 @@ app.get('/' , (req,res)=>{
 
 
 io.on('connection', (socket)=>{
-    console.log('we have a new connection!!!');
 
     socket.on('join',({name,room},callback)=>{
         const {error,user}=addUser({id:socket.id,name,room});
 
 
         if(error) return callback(error);
-        console.log(user)
         socket.join(user.room);
         
 
@@ -38,17 +38,17 @@ io.on('connection', (socket)=>{
     })
 
     socket.on('sendMessage',(message,callback)=>{
-        console.log(socket.id)
         const user=getUser(socket.id);
-        console.log(user)
-        console.log(message);
         io.to(user.room).emit('message',{user:user.name,text:message});
         callback();
     })
 
     socket.on('disconnect', ()=>{
         const user = removeUser(socket.id);
-        console.log('User left !!!!! ')
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+        }
     })
 });
 server.listen(PORT,() => console.log(`Server is running on Port ${PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,17 @@ import {BrowserRouter as Router , Route} from 'react-router-dom';
 
 import Join from './components/Join/Join';
 import Chat from './components/Chat/Chat';
+import Footer from './components/Footer/Footer';
 
 const App = () =>{
     return(
+        <div>
         <Router>
             <Route path="/" exact component={Join}/>
             <Route path="/chat"  component={Chat}/>
         </Router>
+        <Footer/>
+        </div>
     );
 };
 
